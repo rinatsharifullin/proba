db27f5bd7666513a45e5a2295adfccdec8ae2dcc
rinatsharifullin <58179621+rinatsharifullin@users.noreply.github.com>
2020-03-14 09:22:11 +0000

    Add files via upload

--- scripts/basescript.js
+++ scripts/basescript.js
@@ -3,19 +3,39 @@
 
 document.addEventListener('DOMContentLoaded', function(){
 	mobileMenu();
+	openMap();
+	closeMap();
 });
 
 function mobileMenu(){
 	var iconElement = document.getElementById('mobileIcon');
-	console.log(iconElement.innerText == '☰');
 	
 	iconElement.addEventListener('click', function(){
 		if(iconElement.innerHTML=='☰'){
 			document.getElementById('navigation').style.display = 'flex';
 			iconElement.innerHTML='&#88';
+			var iframe = document.getElementById("mymap");iframe.style.display = "none";
 		}else{
 			document.getElementById('navigation').style.display = 'none';
 			iconElement.innerHTML='&#9776';
 		}
 	})
 }
+function openMap(){
+	var iframe = document.getElementById("mymap");
+	var mapIcon = document.getElementById("gmap");
+	mapIcon.addEventListener('click', function(){
+		iframe.style.display='block';
+		
+	})
+}
+
+function closeMap(){
+	var iframe = document.getElementById("mymap");
+	var x = document.getElementsByClassName("close");
+	x[0].addEventListener('click', function(){
+		iframe.style.display = "none";
+	});
+
+}
+
