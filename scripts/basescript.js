2909e5052dda9e18ded833e1ddee060c3d905470
rinatsharifullin <58179621+rinatsharifullin@users.noreply.github.com>
2020-03-15 20:40:57 +0000

    Add files via upload

--- scripts/basescript.js
+++ scripts/basescript.js
@@ -2,11 +2,13 @@
 /* This function provide waiting time till content loaded */
 
 document.addEventListener('DOMContentLoaded', function(){
-	/* menuState(); */
+	menuState();
 	mobileMenu();
 	openMap();
 	closeMap();
-	detectDevice();
+	/* showAddress(); */
+	textAnimation();
+	
 });
 
 
@@ -21,7 +23,7 @@ function mobileMenu(){
 	iconElement.addEventListener('click', function(){
 		if(iconElement.innerHTML=='☰'){
 			navVisibility('visible');
-			iframe = document.getElementById("mymap");iframe.style.display = "none";
+			document.getElementById("mymap").style.display = "none"; /* Hide map when mobile menu open */
 		}else{
 			navVisibility('hidden');
 		}
@@ -38,11 +40,21 @@ function navVisibility(state){
 		document.getElementById('mobileIcon').innerHTML='&#9776';
 	}
 }
-
+/* In case of resizing mobile menu initial state must be display none but desktop version must be visible */
+function menuState(){
+	window.addEventListener("resize", function(){
+		/* If window size over 768px - display nav menu */
+		if (window.innerWidth > '768'){
+			navVisibility('visible');
+		}else{
+			navVisibility('hidden');
+		}
+	});
+}
 
 /* Open map by click social media icon - location, or close map by clicking same icon */
 function openMap(){
-	document.getElementById("gmap").addEventListener('click', function(){
+	document.getElementsByClassName("fa-map-marker-alt")[0].addEventListener('click', function(){
 		if(document.getElementById("mymap").style.display=='block'){
 			document.getElementById("mymap").style.display = "none";
 		}else{	document.getElementById("mymap").style.display='block';
@@ -58,86 +70,32 @@ function closeMap(){
 	});
 }
 
-/* In case of resizing mobile menu initial state must be display none but desktop version must be visible */
-function menuState(){
-	window.addEventListener("resize", function(){
-		/* If window size over 768px - display nav menu */
-		if (window.innerWidth > '768'){
-			navVisibility('visible');
-		}else{
-			navVisibility('hidden');
-		}
-	});
-}
-function detectDevice(){
-	/* window.addEventListener("resize", function(){ */
-		if(window.innerWidth < window.innerHeight){
-			removeClass('myfooter');
-			removeClass('navigation');
-			removeClass('mobileIcon');
-			removeClass('home');
-			removeClass('work');
-			removeClass('blog');
-			removeClass('about');
-			removeClass('contact');
-			
-			/* document.getElementById("desktopStyling").disabled = true; */
-		}else{
-			/* document.getElementById("desktopStyling").disabled = false; */
-			addClass('desktopfooter', 'myfooter');
-			addClass('desktopul', 'navigation');
-			addClass('desktopmobileIcon', 'mobileIcon');
-			addClass('desktopli', 'home');
-			addClass('desktopli', 'work');
-			addClass('desktopli', 'blog');
-			addClass('desktopli', 'about');
-			addClass('desktopli', 'contact');
-			
-	}
-	/* }) */
-}
-
-
-/* Togglr class name */
-function toggleClass(myclass, myid){
-	var element = document.getElementById(myid);
-	console.log(element);
-	if (element.classList) {
-	  element.classList.toggle(myclass);
-	} else {
-	  // For IE9
-	  var classes = element.className.split(" ");
-	  var i = classes.indexOf(myclass);
-
-	  if (i >= 0)
-		classes.splice(i, 1);
-	  else
-		classes.push(myclass);
-		element.className = classes.join(" ");
-	}
-
-}
-
-
-function addClass(myclass, myid) {
-  var element = document.getElementById(myid);
-	console.log(element);
-	if (element.classList) {
-	  element.classList.add(myclass);
-	} else {
-	  // For IE9
-	  var classes = element.className.split(" ");
-	  var i = classes.indexOf(myclass);
-
-	  if (i >= 0)
-		classes.splice(i, 1);
-	  else
-		classes.push(myclass);
-		element.className = classes.join(" ");
-	}
-}
-
-function removeClass(myid) {
-  var element = document.getElementById(myid);
-  element.className = element.className.replace(/\bmystyle\b/g, "");
+/* Show address by click social media icon - address, or hide address by clicking same icon */
+/* function showAddress(){
+	document.getElementsByClassName("fa-at")[0].addEventListener('click', function(){
+		if(document.getElementById("myemail").style.display=='block'){
+			document.getElementById("myemail").style.display = "none";
+		}else{	document.getElementById("myemail").style.display='block';
+				if (window.innerWidth < '768'){navVisibility('hidden');} /* Hide nav menu if opening to prevent overlapping */
+/* 		}
+	})
+}  */
+/* Home page text on scroll animation */
+function textAnimation(){
+	var textElement = document.getElementsByClassName('path');
+	var myimage = document.getElementById('stickyimg');
+	var offset = 0;
+	textElement[0].style.strokeDasharray  = 2000;
+	textElement[0].style.opacity = 0;
+	window.addEventListener('scroll', function(){
+		offset =2000 - window.pageYOffset * 5; 
+		if (offset < 0){
+			offset=0;
+			myimage.style.opacity=1;
+			myimage.style.transition = 'opacity 1s ease';
+		}else{myimage.style.opacity=0;}
+		
+		textElement[0].style.opacity = 1;
+		textElement[0].style.strokeDashoffset = offset; 
+	})
 }
